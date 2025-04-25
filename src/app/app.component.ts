4722e0a4f698423ade41e6c6ccfb4203228bdf40
Evalero80 <167867123+Evalero80@users.noreply.github.com>
2025-04-25 14:19:22 +0200

    prueba_2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RouterOutlet,RouterLink,RouterLinkActive} from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 
+
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,RouterLink,RouterLinkActive,HeaderComponent,FooterComponent],
