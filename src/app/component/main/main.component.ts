adb1ff03b4e7bdf4d9131f034d33e901a02efe4e
Evalero80 <167867123+Evalero80@users.noreply.github.com>
2025-04-30 19:42:29 +0200

    Dia3_imagen colocada con titulo diseños y un croll horizontal

--- src/app/component/main/main.component.ts
+++ src/app/component/main/main.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink,RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
