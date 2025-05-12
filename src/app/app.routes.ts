d0114c26d62f9661c86c112ba262152844c039d0
NoeRC24 <noerodriguezc24@gmail.com>
2025-05-12 18:38:05 +0200

    Main

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { MainNoeComponent } from './main-noe/main-noe.component';
+
+export const appRoutes: Routes = [
+  { path: '', component: MainNoeComponent },
+ 
+];
