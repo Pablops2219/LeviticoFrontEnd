3b95f0a66d6b498c306e01c7e21a2af4364ac30e
Mariio8005 <mariiobenediictomoreno@gmail.com>
2025-05-12 19:05:10 +0200

    Componente note

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ReciepeListComponent } from './reciepe-list/reciepe-list.component';
+import { MainNoeComponent } from './main-noe/main-noe.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'recipes', component: ReciepeListComponent }
+  { path: 'recipes', component: ReciepeListComponent },
+  { path: 'main-noe', component: MainNoeComponent }
 ];
