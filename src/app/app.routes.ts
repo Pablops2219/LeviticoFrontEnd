d73b9d41db2ea6aebaa88fcc630b95a60e6e7d2e
Pablops2219 <pperniassantiago@gmail.com>
2025-05-12 19:11:51 +0200

    merge final

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 
 import { MainComponent } from './component/main/main.component';
 import { ProductosLocalesComponent } from './component/productos-locales/productos-locales.component';
+import { ReciepeListComponent } from './component/reciepe-list/reciepe-list.component';
+import { MainNoeComponent } from './component/main-noe/main-noe.component';
+import { HomeComponent } from './component/home/home.component';
 
 export const routes: Routes = [
-    {path: '', component: MainComponent},
-    {path: 'productos-locales', component: ProductosLocalesComponent},
+  { path: '', component: MainComponent },
+  { path: 'productos-locales', component: ProductosLocalesComponent },
   { path: 'recipes', component: ReciepeListComponent },
-  { path: 'main-noe', component: MainNoeComponent }
-    
+  { path: 'main-noe', component: MainNoeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'reciepe-list', component: ReciepeListComponent },
 ];
